448082023becee7ed15ec2a037d893ea640be208
TTC <banglajoy701@gmail.com>
2021-10-11 20:26:42 -0700

    12 October 2021

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,14 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
+String btnText1="Button One";
+Color btnColor1=Colors.purpleAccent;
+bool imgVis1=false;
+String btnText2="Button Two";
+bool imgVis2=false;
+String imgSrc1="https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510_960_720.jpg";
+double imgHeight=150;
+double imgWidth=200;
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -28,32 +36,93 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text("Maria"),
       ),
       body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            children: [
+          child: Center(
+            child: Column(
+                children: [
+
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            image: DecorationImage(image:
+                            NetworkImage(imgSrc1),fit: BoxFit.cover),
+                            border: Border.all(color: Colors.purple,width: 4),
+                            borderRadius: BorderRadius.all(Radius.circular(50)),
+                          ),
+                          height: imgHeight,
+                           width: imgWidth,
+                            child: Image.network(imgSrc1)),
+                        Container(
+                            height: imgHeight,
+                            width: imgWidth,
+                            child: Image.network(imgSrc1)),
+                        Container(
+                            height: imgHeight,
+                            width: imgWidth,
+                            child: Image.network(imgSrc1)),
+                        Container(
+                            height: imgHeight,
+                            width: imgWidth,
+                            child: Image.network(imgSrc1)),
+                        Container(
+                            height: imgHeight,
+                            width: imgWidth,
+                            child: Image.network(imgSrc1)),
+                      ],
+                    ),
+                  ),
 
 
-              RaisedButton(
-                  child: Text("Button 1"),
-                  onPressed: (){
 
+
+
+
+                  RaisedButton(
+                    color: btnColor1,
+                      child: Text(btnText1),
+                      onPressed: (){
+                        setState(() {
+                          btnColor1=Colors.amberAccent;
+                          btnText1="Button Pressed";
+                          imgVis1=true;
+                        });
+
+                      }),
+                  Visibility(
+                    visible: imgVis1,
+                    child: Container(
+                        height: 500,
+                        width: 400,
+                        child: Image.network(imgSrc1)
+                    ),
+
+                  ),
+
+                  Text("Flower"),
+                  RaisedButton(
+                    child: Text("Button Two"),
+                      onPressed: (){
+                    setState(() {
+                      btnText2="Button Pressed";
+                      imgVis2=true;
+                    });
                   }),
-              Container(
-                  height: 500,
-                  width: 400,
-                  child: Image.network("https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510_960_720.jpg")
-              ),
-              Text("Flower"),
-              Container(
-                height: 600,
-                width: 300,
-                child: Image.asset("assets/images/image1.jpeg"),
-              ),
 
-            ],
+                  Visibility(
+                    visible: imgVis2,
+                    child: Container(
+                      height: 500,
+                      width: 300,
+                      child: Image.asset("assets/images/image1.jpeg"),
+                    ),
+                  ),
+
+                ],
+              ),
+            ),
           ),
-        ),
-      ),
 
     );
   }
